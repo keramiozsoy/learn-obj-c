f510ee7ad446aea7a84d6ba59685546d7afd822d
kerami <keramiozsoy@hotmail.com>
2020-03-18 22:30:20 +0300

    lesson 7

--- learn-obj-c/main.m
+++ learn-obj-c/main.m
@@ -11,38 +11,33 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
-        // sinif olan bir veri tipi ile bir değişken tanımlanırsa önüne * işareti getirilir.
-        // alloc hafizadan yer ayiriyor
-        // @ string değerini karşılıyor
         
+        // for each
         
-        // NSArray         listesi degismez dizi
-        // NSMutableArray  listesi degisebilen dizi
+        NSMutableArray *array  = [[NSMutableArray alloc] init];
+        [array addObject:@"val1"];
+        [array addObject:@"val2"];
+        [array addObject:@"val3"];
         
-        NSMutableArray *dizi = [[NSMutableArray alloc] init];
-        NSMutableArray *dizi2 = [NSMutableArray array];
-        
-         
-        [dizi addObject:@"eleman1"];
-        [dizi addObject:@"eleman2"];
-        [dizi addObject:@"eleman3"];
-        
-        [dizi2 addObject:@"eleman1"];
-        [dizi2 addObject:@"eleman2"];
-        [dizi2 addObject:@"eleman3"];
-        
-        NSLog(@" ikinci eleman --> %@  ", [dizi objectAtIndex:1]);
-        NSLog(@" ikinci eleman --> %@  ", [dizi2 objectAtIndex:1]);
+        for (NSString *temp in array) {
+            NSLog(@" %@ ", temp);
+        }
         
         NSLog(@" -- ");
         
+        // dictionary aslında key-value map tir. key ile objelere ulaşılır
         
-        NSArray *degismezDizi = [[NSArray alloc] initWithObjects:@"eleman1",@"eleman2", nil];
+        NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
         
-        //  [degismezDizi addObject:@"eleman0"]; // bu satır çalışmaz
+        [dic setObject:@"deneme1" forKey:@"ilk"];
         
+        NSLog(@" dic --> %@ ", [dic objectForKey:@"ilk"]);
+        NSLog(@" dic --> %@ ", [dic objectForKey:@"iki"]);
         
     
+        NSLog(@" -- ");
+        
+        
         
         
     }

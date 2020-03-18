21766bc62b9e123000c50a45520b50a88d8a31ea
kerami <keramiozsoy@hotmail.com>
2020-03-18 17:15:12 +0300

    lesson 1

---
+++ learn-obj-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  learn-obj-c
+//
+//  Created by Mac on 18.03.2020.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

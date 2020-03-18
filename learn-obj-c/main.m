98f4d6d92e69fcefd3e8b204a25c671b91348580
kerami <keramiozsoy@hotmail.com>
2020-03-18 17:21:24 +0300

    lesson 2

--- learn-obj-c/main.m
+++ learn-obj-c/main.m
@@ -12,6 +12,32 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        // int, double, float, char, BOOL, string, nslog,
+        
+        int sayi1 = 10;
+        sayi1 = 20;
+        
+        float sayi2 = 123.12;
+        sayi2 = sayi2 + sayi2;
+        
+        double sayi3 = 1231231.1231231231;
+        
+        BOOL bl = YES;
+        bl = NO;
+        
+        NSString *str = @" deneme ";
+        NSLog(@"lol log : sayi1: %d ", sayi1);
+        
+        // bu bir yorum satiri
+        
+        /*
+         
+         Bir çok satırlı yorum satiri
+         
+         */
+        
+        
     }
     return 0;
 }

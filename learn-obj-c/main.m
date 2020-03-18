ac6b214c1450405e7b0f81f9531049d95980afc1
kerami <keramiozsoy@hotmail.com>
2020-03-18 18:07:34 +0300

    lesson 4

--- learn-obj-c/main.m
+++ learn-obj-c/main.m
@@ -11,32 +11,60 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
+        
+    // String temel tip değildir, bir siniftir
+
+       
+     // Değeri degisebilen string
+        
+        NSMutableString *strMutable = [[NSMutableString alloc] initWithString:@"denemeMutable1"];
+        
+        NSMutableString *strMutable2 = [NSMutableString stringWithString:@"denemeMutable2"];
+        
+        NSLog(@" strMutable -- %@ ",strMutable);
+        NSLog(@" strMutable2 -- %@ ",strMutable2);
     
-        int sayi = 10;
-        if (sayi == 10) {
-            NSLog(@" sayi esit");
-        }else if (sayi > 10){
-            NSLog(@" sayi buyuk");
-        }else {
-            NSLog(@" sayi kucuk");
+    // Değeri degisemeyen string
+        
+        NSString *str = @"deneme";
+
+        NSString *str2 = [NSString stringWithFormat:@" %d %@ ",12,str];
+        
+        NSLog(@" str -- %@ ",str);
+        NSLog(@" str2 -- %@ ",str2);
+        
+    //  denemeler
+        
+        
+        [strMutable appendString:str2]; // strMutable + str2
+        NSLog(@"strMutable + str2 --  %@", strMutable);
+        
+        [str stringByAppendingString:str2];
+        NSLog(@"str + str2 --  %@",str); // sonuc degismedi
+        
+    // String karsilastirma
+        
+        
+        /*
+         siniflar dogrudan karsilastirilamaz
+        
+        if (@"k" == @"k"){
+         calismaz
         }
+         
+        */
+         
+        NSString *str3 = @"de";
+        NSString *str4 = @"de";
         
-        
-        int swichSayi = 30;
-        
-        switch (swichSayi) {
-            case 20:
-                NSLog(@"sayi 10 a esittir");
-                break;
-            case 10:
-                NSLog(@"sayi 10 a esittir");
-            break;
-                
-            default:
-                NSLog(@"sayi degeri : %d ", swichSayi);
-                break;
+        BOOL bl = NO;
+        if([str3 isEqualToString:str4]){
+            bl = YES;
         }
+        NSLog(@" %d ",bl);
+        NSLog(bl ? @"Yes" : @"No");
         
+    
         
     }
     return 0;

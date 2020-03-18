db02e38a2b093041f0fe6570f4dd7d06d501638e
kerami <keramiozsoy@hotmail.com>
2020-03-18 18:19:15 +0300

    lesson 5

--- learn-obj-c/main.m
+++ learn-obj-c/main.m
@@ -11,60 +11,43 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
+        // while
         
-    // String temel tip değildir, bir siniftir
-
-       
-     // Değeri degisebilen string
-        
-        NSMutableString *strMutable = [[NSMutableString alloc] initWithString:@"denemeMutable1"];
-        
-        NSMutableString *strMutable2 = [NSMutableString stringWithString:@"denemeMutable2"];
-        
-        NSLog(@" strMutable -- %@ ",strMutable);
-        NSLog(@" strMutable2 -- %@ ",strMutable2);
-    
-    // Değeri degisemeyen string
-        
-        NSString *str = @"deneme";
-
-        NSString *str2 = [NSString stringWithFormat:@" %d %@ ",12,str];
-        
-        NSLog(@" str -- %@ ",str);
-        NSLog(@" str2 -- %@ ",str2);
-        
-    //  denemeler
-        
-        
-        [strMutable appendString:str2]; // strMutable + str2
-        NSLog(@"strMutable + str2 --  %@", strMutable);
-        
-        [str stringByAppendingString:str2];
-        NSLog(@"str + str2 --  %@",str); // sonuc degismedi
-        
-    // String karsilastirma
-        
-        
-        /*
-         siniflar dogrudan karsilastirilamaz
-        
-        if (@"k" == @"k"){
-         calismaz
+        int i = 0;
+        while (i<10) {
+            
+            if (i == 4){
+                break;
+            }
+            
+            i = i + 1;
+            NSLog(@" %d ",i);
+            
         }
-         
-        */
-         
-        NSString *str3 = @"de";
-        NSString *str4 = @"de";
         
-        BOOL bl = NO;
-        if([str3 isEqualToString:str4]){
-            bl = YES;
+        NSLog(@" -- ");
+     
+        
+        // do while
+        
+        int j = 2;
+        do {
+            j++;
+            NSLog(@" %d ",j);
+        } while (j <5);
+        
+        NSLog(@" -- ");
+        
+        // for
+        
+        for (int i = 1; i<3; i++){
+            NSLog(@"%d",i);
         }
-        NSLog(@" %d ",bl);
-        NSLog(bl ? @"Yes" : @"No");
+        
+        NSLog(@" -- ");
         
     
+        
         
     }
     return 0;
